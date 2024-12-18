9aad0b77a99663a313e56216a22c52b7d9964d46
qiankun <iamfuryman@sina.com>
2024-12-18 12:07:26 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+const compony = [
+  {
+    name: "ok币所",
+  },
+  {
+    name: "商汤科技",
+  },
+  {
+    name: "百度科技",
+  },
+];
+</script>
+
+<template>
+  <div>
+    <div v-for="item in compony">
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
